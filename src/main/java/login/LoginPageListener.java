cf767fe46309221d4bdc7e98dcc22d79f9219c81
sakanobu <grapejuice100percent@gmail.com>
2023-01-21 22:17:57 +0900

    login_idフィールドとpasswordフィールドの両方が入力されていない場合での挙動の実装

--- src/main/java/login/LoginPageListener.java
+++ src/main/java/login/LoginPageListener.java
@@ -12,9 +12,18 @@ public class LoginPageListener implements ActionListener {
 
   @Override
   public void actionPerformed(ActionEvent e) {
+    String loginId = loginPagePanel.getLoginId();
+    String password = loginPagePanel.getPassword();
+
     if (e.getActionCommand().equals("ログイン")) {
-      LoginAppFrame loginAppFrame = (LoginAppFrame) loginPagePanel.getTopLevelAncestor();
-      loginAppFrame.change(new ResultPagePanel());
+      // login_id 入力フィールドと password 入力フィールドが両方とも空の場合
+      if (loginId.equals("") && password.equals("")) {
+        loginPagePanel.setResultLabelText("login_id と password が空です。");
+      }
+
+      // ResultPagePanelへの画面遷移用のコード
+      // LoginAppFrame loginAppFrame = (LoginAppFrame) loginPagePanel.getTopLevelAncestor();
+      // loginAppFrame.change(new ResultPagePanel());
     }
   }
 }
