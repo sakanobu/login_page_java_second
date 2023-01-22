ad82af9353a5a0cf37906eaffdeb09d695be2a9d
sakanobu <grapejuice100percent@gmail.com>
2023-01-22 15:50:17 +0900

    UserDaoクラスにfindAllメソッドを追加

--- src/main/java/login/UserDao.java
+++ src/main/java/login/UserDao.java
@@ -6,6 +6,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class UserDao {
   private final String url;
@@ -17,6 +18,14 @@ public class UserDao {
 
   public static void main(String[] args) {
     UserDao userDao = new UserDao();
+
+    System.out.println("\n↓ userDao.findAll()");
+    for (User user : userDao.findAll()
+    ) {
+      System.out.printf("%d, %s, %d, %b, %s\n".formatted(user.userId(), user.name(), user.age(),
+          user.retired(),
+          user.role()));
+    }
 
     System.out.println("\n↓ userDao.findUserByLoginIdAndPassword(\"login1\", \"password1\")");
     User user1 = userDao.findUserByLoginIdAndPassword("login1", "password1");
@@ -40,6 +49,56 @@ public class UserDao {
     this.url = Dotenv.load().get("MYSQL_URL");
     this.dbUserName = Dotenv.load().get("MYSQL_USER_NAME");
     this.dbPassword = Dotenv.load().get("MYSQL_PASSWORD");
+  }
+
+  public ArrayList<User> findAll() {
+    ArrayList<User> allUserList = new ArrayList<>();
+    String sql = """
+        SELECT
+          u.id,
+          u.name,
+          u.age,
+          u.is_retired,
+          r.name
+        FROM
+          users AS u
+          INNER JOIN roles AS r ON u.role_id = r.id
+          INNER JOIN user_auths AS ua ON u.id = ua.user_id;
+        """;
+
+    try {
+      con = DriverManager.getConnection(url, dbUserName, dbPassword);
+      ps = con.prepareStatement(sql);
+      rs = ps.executeQuery();
+
+      while (rs.next()) {
+        int userId = rs.getInt("u.id");
+        String name = rs.getString("u.name");
+        int age = rs.getInt("u.age");
+        String role = rs.getString("r.name");
+        boolean retired = rs.getBoolean("u.is_retired");
+
+        allUserList.add(new User(userId, name, age, retired, role));
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        if (rs != null) {
+          rs.close();
+        }
+        if (ps != null) {
+          ps.close();
+        }
+        if (con != null) {
+          con.close();
+        }
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+    }
+
+    return allUserList;
   }
 
   public User findUserByLoginIdAndPassword(String loginId, String password) {
