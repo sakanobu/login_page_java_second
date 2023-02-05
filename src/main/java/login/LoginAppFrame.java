c5f544ca8ed74a1db35b0fced5a8e10c06d74658
sakanobu <grapejuice100percent@gmail.com>
2023-02-05 13:47:23 +0900

    画面遷移ごとにページタイトルを"ログイン画面"と"結果画面"と切り替わるようにした

--- src/main/java/login/LoginAppFrame.java
+++ src/main/java/login/LoginAppFrame.java
@@ -5,13 +5,15 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class LoginAppFrame extends JFrame {
+  private String pageTitle = "ログイン画面";
+
   public static void main(String[] args) {
     LoginAppFrame frame = new LoginAppFrame();
     frame.setVisible(true);
   }
 
   public LoginAppFrame() {
-    setTitle("ログイン画面");
+    setTitle(pageTitle);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setLocationRelativeTo(null);
     int width = 720;
@@ -24,9 +26,12 @@ public class LoginAppFrame extends JFrame {
   }
 
   public void change(JPanel panel) {
+    pageTitle = pageTitle.equals("ログイン画面") ? "結果画面" : "ログイン画面";
+
     getContentPane().removeAll();
 
     super.add(panel);
+    super.setTitle(pageTitle);
 
     revalidate();
     repaint();
